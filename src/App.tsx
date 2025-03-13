00062c5e612fb4f04da7ae10792b8c81485e16c9
Zsolt Dobak <zsolt.dobak@oracle.com>
2025-03-13 17:33:13 +0100

    Add menu and contacts page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import "./App.scss"
 import LandingPage from './components/LandingPage';
 import Contacts from './components/Contacts';
 import Programs from './components/Programs';
 import Treatments from './components/Treatments';
 import { BrowserRouter, Routes, Route } from "react-router";
+import {Menu, MenuItem} from "./components/Menu";
 
+const menuItems: MenuItem[] = [
+  { id: 1, label: 'Kezdőlap', href: '/' },
+  { id: 3, label: 'Online programok', href: '/online-programs' },
+  { id: 4, label: 'Egyéni kezelések', href: '/individual-treatments' },
+  { id: 2, label: 'Kapcsolat', href: '/contacts' },
+];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Menu items={menuItems} />
       </header>
       <BrowserRouter>
         <Routes>
--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import "./ContactsPage.scss"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebook, faInstagram, faTiktok, faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
 
 const Contacts: React.FC = () => {
   return (
-    <div className="landing-page">
-      Kapcsolat
+    <div className="contacts-page">
+      <h1>Ha szükséged van a segítségemre, itt megtalálsz!</h1>
+
+      <div className="contacts-wrapper">
+        <span className="link-container"><a href={"mailto: kirillaphysio@gmail.com"}><FontAwesomeIcon icon={faEnvelope} />Ha bármi kérdésed van, írj egy emailt nekem! (kirillaphysio@gmail.com)</a></span>
+        <span className="link-container"><a href={"tel: +36202668447"}><FontAwesomeIcon icon={faPhone} />Vagy csörögj rám munkaidöben (+36202668447)</a></span>
+      </div>
+
+      <label className="social-label">Vedd fel velem a kapcsolatot a közösségi média oldalaimon!</label>
+      <div className="social-wrapper">
+        <FontAwesomeIcon icon={faFacebook} />
+        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon icon={faTiktok} />
+        <FontAwesomeIcon icon={faYoutube} />
+      </div>
     </div>
   );
 };
