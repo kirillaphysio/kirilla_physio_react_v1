ea937c636203d14a0a4e9e2a06fea6d602c3d7b0
Zsolt Dobak <zsolt.dobak@oracle.com>
2025-03-15 19:19:40 +0100

    fix eslint issues

--- src/components/social-contacts/Social.tsx
+++ src/components/social-contacts/Social.tsx
@@ -8,10 +8,10 @@ const Social = () => {
     <div className={"social"}>
       {!isMobile && <label className="social-label">Vedd fel velem a kapcsolatot a közösségi média oldalaimon!</label>}
       <div className="social-wrapper">
-        <a className="social-link" href="https://www.facebook.com/kirillaphysio/" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
-        <a className="social-link" href="https://www.instagram.com/kirilla_physio/" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
-        <a className="social-link" href="https://www.tiktok.com/@kirilla_physio" target="_blank"><FontAwesomeIcon icon={faTiktok} /></a>
-        <a className="social-link" href="https://www.youtube.com/channel/UCN9ZM4g1KHw_8GTmYq9cG2g" target="_blank"><FontAwesomeIcon icon={faYoutube} /></a>
+        <a className="social-link" href="https://www.facebook.com/kirillaphysio/" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
+        <a className="social-link" href="https://www.instagram.com/kirilla_physio/" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
+        <a className="social-link" href="https://www.tiktok.com/@kirilla_physio" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faTiktok} /></a>
+        <a className="social-link" href="https://www.youtube.com/channel/UCN9ZM4g1KHw_8GTmYq9cG2g" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faYoutube} /></a>
       </div>
     </div>
   );
--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -13,7 +13,7 @@ const Contacts: React.FC = () => {
         <div className="link-container">
           <a href={"https://maps.app.goo.gl/xPYxjWL8BTMPRfZb9"}><FontAwesomeIcon icon={faMapLocationDot}/>
             A kezelés helyszíne: For Your Move and Health Centrum (Budapest, I. kerület, Ostrom utca 31.)</a>
-          {!isMobile && <iframe
+          {!isMobile && <iframe title="Google maps"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d696.6281264758042!2d19.026790260569324!3d47.50712550111642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741df1260bb4987%3A0x989f504579199ed!2sFor%20Your%20Move%20and%20Health%20Centrum!5e0!3m2!1sen!2shu!4v1742048603716!5m2!1sen!2shu"
             width="100%" height="450" style={{border: 0}} allowFullScreen={true} loading="lazy"
             referrerPolicy="no-referrer-when-downgrade">
