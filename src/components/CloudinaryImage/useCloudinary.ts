5a07718016663f216008463e745f0cd6b5ad45bd
Zsolt Dobak <zsolt.dobak@oracle.com>
2025-04-07 18:50:36 +0200

    fix linter issue

--- src/components/CloudinaryImage/useCloudinary.ts
+++ src/components/CloudinaryImage/useCloudinary.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Cloudinary } from '@cloudinary/url-gen';
 
 export const useCloudinary = () => {
-  const [instance, setInstance] = useState((window as any).cloudinary ?? new Cloudinary({ cloud: { cloudName: 'dcwv2corw' } }));
+  const [instance] = useState((window as any).cloudinary ?? new Cloudinary({ cloud: { cloudName: 'dcwv2corw' } }));
 
   useEffect(() => {
     (window as any).cloudinary = instance
