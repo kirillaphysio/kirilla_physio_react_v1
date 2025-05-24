5fbf16e4d9c0f5f5892a32d139cf11adbc761e70
Zsolt Dobak <zsolt.dobak@oracle.com>
2025-05-24 15:54:30 +0200

    Fix image sizing

--- src/components/CloudinaryImage/CloudinaryImage.tsx
+++ src/components/CloudinaryImage/CloudinaryImage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { auto } from '@cloudinary/url-gen/actions/resize';
-import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
 import { AdvancedImage } from '@cloudinary/react';
 import {useCloudinary} from "./useCloudinary";
 
@@ -15,17 +14,15 @@ const DEFAULT_SIZE = 300;
 const CloudinaryImage = (props: any) => {
   const cld = useCloudinary();
 
-  console.log(props)
-
   const img = cld
     .image(props.imageId)
     .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
     .quality('auto')
-    .resize(auto().gravity(autoGravity()).width(props.width ?? DEFAULT_SIZE).height(props.height ?? DEFAULT_SIZE)); // Transform the image: auto-crop to square aspect_ratio
+    .resize(auto().width(props.width ?? DEFAULT_SIZE).height(props.height ?? DEFAULT_SIZE)); // Transform the image: auto-crop to square aspect_ratio
 
   return (<AdvancedImage cldImg={img}
                          loading="lazy"
-                         fetchpriority="high"
+                         fetchPriority="high"
                          onError={(event: any) => {event.target.src = `https://placehold.co/${props.width ?? DEFAULT_SIZE}/5F3D44/F8EFF1?text=A+k%C3%A9p+nem+el%C3%A9rhet%C3%B6`}}
   />);
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './LandingPage.scss';
+import CloudinaryImage from "../components/CloudinaryImage/CloudinaryImage";
 
 const LandingPage: React.FC = () => {
   return (
     <div className="landing-page">
       <section className="hero-section">
         <h1>Üdvözlünk a weboldalunkon!</h1>
+        <CloudinaryImage imageId="kezdőlap__u1ybav" />
         <p>Fedezd fel, amit kínálunk.</p>
         <button className="cta-button">Ismerd meg most!</button>
       </section>
