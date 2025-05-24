87acd6c26f3741010c1b5b68fc280a34e7dbf6ea
Zsolt Dobak <zsolt.dobak@oracle.com>
2025-05-24 16:49:15 +0200

    Add welcome page content

---
+++ src/components/opinions/aboutMe.ts
@@ -0,0 +1,26 @@
+export const opinions = [
+  {
+    author: "B. Katalin",
+    description: "\"Rékát csak ajánlani tudom, nekem sokféle gerinc és mozgásszervi problémám van, amiken a kezelései sokat segítenek, nagyon profi szakember. A legszimpatikusabb benne, hogy a mai fiatalokhoz képest nagyon céltudatos, folyamatosan képezi magát. 🥰 Mindig kedves és nyugodtság árad belőle, ami a fizikai gyógyulás mellett nagyon pozitív plusz dolog nekem 🥰\"\n"
+  },
+  {
+    author: "T. Fanni",
+    description: "Rékát mindenkinek csak ajánlani tudom, nagyon profi, komolyan veszi a problémákat és mindent belead abba, hogy az összes hozzá forduló páciensének a lehető leghatásosabb, személyre szabottabb kezelést biztosítsa. Folyamatosan fejleszti magát különböző képzéseken, hogy minél szélesebb körben tudja a páciensek igényeihez igazítani a tudását. Nála garantáltan a páciensek élveznek prioritást! Nagyon kedves és türelmes, emellett mind szakmailag, mind emberileg motiváló is, aki teheti őt válassza, mert a legjobb!🙏🏻🤍"
+  },
+  {
+    author: "K. Júlia",
+    description: "Rékának egész életemben hálás leszek!🙏🙏❤️❤️ Neki köszönhető, hogy elkerültem 2 műtétet. Szerintem nagyobb tudással rendelkezik, mint némelyik orvos, profin és szakszerűen látja el a hozzá forduló pácienseket.\n" + "Mindenkinek bátran merem ajánlani!😊😊"
+  },
+  {
+    author: "G. Zsuzsanna",
+    description: "Réka nagyon kedves és profi, munkája során mindig a maximumot adja. Nagyon odafigyel minden apró panaszra, sokat segített nekem is. Bármilyen problémával nyugodtan lehet hozzá fordulni, nagyon jó szívvel ajánlom😊!"
+  },
+  {
+    author: "H. Anna",
+    description: "Réka nagyon kompetensen, odafigyelően végzi a munkáját és odafigyel arra is, hogy folyamatosan képezze magát. Csak ajánlani tudom!"
+  },
+  {
+    author: "P. Martin",
+    description: "Réka igazán jó szakember, felkészült, figyelmes és segítőkész. Sokféle mozgásszervi és egyéb problémán is tud segíteni, még a stresszkezelésben is. Nekem helyretette a hónapok óta nem múló csuklófájdalmam. 😇"
+  }
+]
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,27 +1,78 @@
 import React from 'react';
 import './LandingPage.scss';
+import 'swiper/scss';
+import 'swiper/scss/autoplay';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
 import CloudinaryImage from "../components/CloudinaryImage/CloudinaryImage";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {isMobile} from "react-device-detect";
+import {opinions} from "../components/opinions/aboutMe";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination, A11y } from 'swiper/modules';
+import {nanoid} from "nanoid";
 
 const LandingPage: React.FC = () => {
   return (
     <div className="landing-page">
       <section className="hero-section">
-        <h1>Üdvözlünk a weboldalunkon!</h1>
+        <h1>Üdvözöllek a weboldalamon! </h1>
+        <p>Kirilla Réka vagyok, elhivatott gyógytornász-fizioterapeuta.</p>
         <CloudinaryImage imageId="kezdőlap__u1ybav" />
-        <p>Fedezd fel, amit kínálunk.</p>
-        <button className="cta-button">Ismerd meg most!</button>
+        <p>Küldetésem, hogy segítsek neked megszabadulni a fájdalmaktól, helyreállítani a mozgásképességed, és visszanyerni életminőséged. Módszereim között a gyógytorna, manuálterápia, valamint egyéb kiegészítő kezelések állnak, melyek egyaránt támogatják a fájdalomcsillapítást, a regenerálódást és a prevenciót. </p>
+        <h3>Miért válassz engem?</h3>
+        <ul>
+          <li><FontAwesomeIcon icon={faCheck} />Szakértői tapasztalat és folyamatosan frissített tudás</li>
+          <li><FontAwesomeIcon icon={faCheck} />Személyre szabott, hatékony kezelések holisztikus szemléletmóddal</li>
+          <li><FontAwesomeIcon icon={faCheck} />Empatikus és figyelmes megközelítés</li>
+        </ul>
       </section>
       <section className="hero-section">
-        <h1>Üdvözlünk a weboldalunkon!</h1>
-        <p>Fedezd fel, amit kínálunk.</p>
+        <h1>Hogyan tudok segíteni? </h1>
+        <button className="cta-button">Online programok</button>{/* TODO kartya gomb + description*/}
+        <button className="cta-button">Egyéni kezelés</button>
       </section>
       <section className="hero-section">
-        <h1>Üdvözlünk a weboldalunkon!</h1>
-        <p>Fedezd fel, amit kínálunk.</p>
+        <h1>Rólam:</h1>
+        <p>A mozgás és az egészségtudatosság már kislány koromtól kezdve központi eleme volt az életemnek. Sok sportágban kipróbáltam magam, kezdve az akrobatikával, az aerobikkal, a zumbával, majd később a futás, TRX és funkcionális edzés irányába mentem el, valamint egy ideig az erőemelésben is kipróbáltam magam. Jelenleg pedig a testépítés, és a tánc, amit rendszeresen végzek.</p>
+        <p>A mozgás szeretete, és a vágy, hogy segítsek más embereken egyértelművé tette számomra, hogy gyógytornász szeretnék lenni. A 4 éves gyógytornász-fizioterapeuta képzést a Pécsi Tudományegyetemen végeztem el.</p>
+        <p>Diplomám megszerzése után egy gerincambulancián kezdtem el dolgozni, ahol egyéni gyógytornával kezeltem gerinc eredetű panasszal rendelkező pácienseket, és hamar elkezdtem tanfolyamokra járni, hogy a gyógytornán túl egyéb eszközökkel is tudjam segíteni a pácienseim gyógyulását. Emellett dolgoztam egy mozgásközpontban, ahol csoportos gerinctornákat tartottam. Egy másik jógaközpontban pedig anatómiát oktattam leendő jógaoktatók számára. </p>
+        <p>A kellő mennyiségű szakmai tapasztalat, és 25+ elvégzett tanfolyam után 2023-ban elindítottam a saját vállalkozásomat, mely során egyre több kiegészítő terápiás eszközt (manuálterápia, köpöly, visceralis terápia stb.) alkalmaztam, és a mai napig is folyamatosan bővítem a terápiás eszköztáramat. </p>
+        <p>A mozgás mellett elengedhetetlennek tartom, hogy odafigyeljünk az étkezésünkre, és a mentális egészségünkkel is aktívan foglalkozzunk. </p>
+        <p>A kompetenciahatáraimat betartva ebben is segítem a pácienseimet, illetve igyekszem jó példát mutatni a saját életstílusommal: tisztán étkezem, rendszeresen meditálok, és sok önfejlesztő tartalmat fogyasztok.</p>
       </section>
       <section className="hero-section">
-        <h1>Üdvözlünk a weboldalunkon!</h1>
-        <p>Fedezd fel, amit kínálunk.</p>
+        <h1>Rólam mondták</h1>
+
+        <Swiper
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: true,
+          }}
+          grabCursor={true}
+          loop={true}
+          modules={[Autoplay, Navigation, Pagination, A11y]}
+          navigation
+          pagination={{ dynamicBullets: true }}
+          slidesPerView={isMobile ? 1 : 3}
+          spaceBetween={32}
+          style={{
+            "--swiper-navigation-size": "22px",
+            "--swiper-navigation-color": "#F8EFF1",
+            "--swiper-pagination-color": "#F8EFF1",
+            "--swiper-pagination-bullet-inactive-color": "#F8EFF1"
+          }}
+        >
+          {opinions.map((opinion: any) => (<SwiperSlide key={nanoid()} className={`opinion ${isMobile ? "mobile" : "desktop"}`}>
+            <p className="message">{opinion.description}</p>
+            <div className="author">{opinion.author}</div>
+          </SwiperSlide>))}
+        </Swiper>
+      </section>
+      <section className="hero-section">
+        <h1>Általam elvégzett képzések</h1>
       </section>
     </div>
   );
