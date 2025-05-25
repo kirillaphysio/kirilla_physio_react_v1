d1c9452bea8cc56a56e9d19b3a0f8c079498df1e
Zsolt Dobak <zsolt.dobak@oracle.com>
2025-05-25 12:18:21 +0200

    scroll to top on every nav route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import "./App.scss"
 import LandingPage from './pages/LandingPage';
 import Contacts from './pages/Contacts';
 import Programs from './pages/Programs';
 import Treatments from './pages/Treatments';
 import Therapy from './pages/Therapy';
-import { HashRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route, useLocation } from "react-router";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import BackToTopButton from "./components/backToTopButton/BackToTopButton";
 
 // TODO add fallback route and page
 
+const ScrollToTopWrapper = ({children}: any) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+}
+
 function App() {
   return (
     <div className="app">
       <HashRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/online-programs" element={<Programs />} />
-          <Route path="/individual-treatments" element={<Treatments />} />
-          <Route path="/therapy/:therapyId" element={<Therapy />} />
+        <ScrollToTopWrapper>
+          <Header />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/online-programs" element={<Programs />} />
+            <Route path="/individual-treatments" element={<Treatments />} />
+            <Route path="/therapy/:therapyId" element={<Therapy />} />
 
-          <Route element={<div className="other-page">Ez az oldal nem található</div>} />
-        </Routes>
-        <BackToTopButton />
-        <Footer />
+            <Route element={<div className="other-page">Ez az oldal nem található</div>} />
+          </Routes>
+          <BackToTopButton />
+          <Footer />
+        </ScrollToTopWrapper>
       </HashRouter>
     </div>
   );
